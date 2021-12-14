df439b4a152a76ab786560eee406d5006a8d6cc1
Brian Miner <brian@fourminers.com>
2021-12-14 14:04:47 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Eye Tracking Exercise
+<p>Full Stack Development with MERN Certification: Week 9 Real Time Bus Tracking Exercise.</p>
+<h2>Features</h2>
+<ul>
+<li>The Realtime Bus Tracker displays current bus locations that travel between Harvard and MIT.  Status is updated every 15 seconds.</li>
+</ul>
+<h2>Instructions</h2>
+<ol>
+<li>Fork to your Github page</li>
+<li>Update the Mapbox Token with your own</li>
+<li>Clone Github page to your local work environment</li>
+<li>Open the index file using a browser</li>
+<li>Click on the "Click to Track Busses" button</li>
+</ol>
+<h2>Roadmap</h2>
+<ul>
+  <li>Looking to add bus capacity status</li>
+  <li>Looking to add traffic information</li>
+</ul>
+<h2>License</h2>
+  <p>See Attached MIT License</p>
---
+++ mapanimation.js
@@ -0,0 +1,59 @@
+//MapBox Token (SHould be replaced with your own token/key)
+ mapboxgl.accessToken = 'pk.eyJ1IjoiYmVlbWFuNDAxIiwiYSI6ImNrd2ptcGN3dTB5dWUyd2w1M3Fqc3p4MDUifQ.jvuS1yZeBY_c9bDluTnmeg';
+
+
+ //Generate new map
+ const map = new mapboxgl.Map({
+  container: 'map', // container ID
+  style: 'mapbox://styles/mapbox/streets-v11', // style URL
+  center: [-71.104081, 42.365554], // starting position [lng, lat]
+  zoom: 13 // starting zoom
+});
+
+
+// Function that run the buses
+async function runBustracker(){ 
+ const buses = await getBusLocations();
+
+ buses.forEach( bus => {
+ const markerObj = getMarker(bus.id);        
+  if(markerObj){
+      const marker = Object.values(markerObj)[0];
+      moveMarker(marker, bus);
+  }else{    
+    addMarker(bus, bus.id);
+  }    
+});     
+setTimeout(runBustracker, 15000); //Adjust as needed to speed up refresh
+}
+
+// Gather data from MBTA
+async function getBusLocations(){
+const url = 'https://api-v3.mbta.com/vehicles?filter[route]=1&include=trip';
+const response = await fetch(url);
+const json     = await response.json();
+return json.data;
+}
+// Add Bus Marker
+let markers = [];
+function addMarker(bus, id) {
+const marker = new mapboxgl.Marker()
+.setLngLat([bus.attributes.longitude, bus.attributes.latitude])
+.addTo(map);
+const element = {};
+element.marker = marker;
+element.id = id
+markers.push(element);   
+}
+
+// Move Bus Marker
+function moveMarker(marker, bus) {
+marker.setLngLat([bus.attributes.longitude, bus.attributes.latitude])
+}
+// Get Marker % Bus Id
+function getMarker(busId) {
+const element = markers.find(item => 
+ item.id === busId
+);
+return element;
+}
