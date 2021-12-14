d5d45466eb3c404e4c696986b227a92f1a0c55a3
Brian Miner <brian@fourminers.com>
2021-12-14 14:28:25 -0800

    Code formatting updates

--- mapanimation.js
+++ mapanimation.js
@@ -4,45 +4,45 @@
 
  //Generate new map
  const map = new mapboxgl.Map({
-  container: 'map', // container ID
-  style: 'mapbox://styles/mapbox/streets-v11', // style URL
-  center: [-71.104081, 42.365554], // starting position [lng, lat]
-  zoom: 13 // starting zoom
+    container: 'map', // container ID
+    style: 'mapbox://styles/mapbox/streets-v11', 
+    center: [-71.104081, 42.365554], 
+    zoom: 13 
 });
 
 
 // Function that run the buses
 async function runBustracker(){ 
- const buses = await getBusLocations();
+    const buses = await getBusLocations();
 
- buses.forEach( bus => {
- const markerObj = getMarker(bus.id);        
-  if(markerObj){
-      const marker = Object.values(markerObj)[0];
-      moveMarker(marker, bus);
-  }else{    
-    addMarker(bus, bus.id);
-  }    
+    buses.forEach( bus => {
+        const markerObj = getMarker(bus.id);        
+            if(markerObj){
+                const marker = Object.values(markerObj)[0];
+                moveMarker(marker, bus);
+            }else{    
+                 addMarker(bus, bus.id);
+                }    
 });     
 setTimeout(runBustracker, 15000); //Adjust as needed to speed up refresh
 }
 
 // Gather data from MBTA
 async function getBusLocations(){
-const url = 'https://api-v3.mbta.com/vehicles?filter[route]=1&include=trip';
-const response = await fetch(url);
-const json     = await response.json();
+    const url = 'https://api-v3.mbta.com/vehicles?filter[route]=1&include=trip';
+    const response = await fetch(url);
+    const json     = await response.json();
 return json.data;
 }
 // Add Bus Marker
 let markers = [];
 function addMarker(bus, id) {
-const marker = new mapboxgl.Marker()
-.setLngLat([bus.attributes.longitude, bus.attributes.latitude])
-.addTo(map);
-const element = {};
-element.marker = marker;
-element.id = id
+    const marker = new mapboxgl.Marker()
+        .setLngLat([bus.attributes.longitude, bus.attributes.latitude])
+        .addTo(map);
+    const element = {};
+    element.marker = marker;
+    element.id = id
 markers.push(element);   
 }
 
@@ -50,10 +50,11 @@ markers.push(element);
 function moveMarker(marker, bus) {
 marker.setLngLat([bus.attributes.longitude, bus.attributes.latitude])
 }
-// Get Marker % Bus Id
+
+// Get Bus Id
 function getMarker(busId) {
-const element = markers.find(item => 
- item.id === busId
-);
+    const element = markers.find(item => 
+        item.id === busId
+    );
 return element;
 }
